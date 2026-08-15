694529de274bd531dcbc9e6553d29afe520e69fa
Jason Holloway <miceeatnicerice@hotmail.com>
2026-08-15 20:49:54 +0100

    more cache

--- cache.sh
+++ cache.sh
@@ -4,8 +4,12 @@ shopt -s extglob
 source "${VARS_PATH:-.}/common.sh"
 
 cacheDir="$HOME/.vars/cache"
+blockDir="$cacheDir/blocks"
 dataDir="$cacheDir/data"
-mkdir -p $dataDir
+mkdir -p $blockDir $dataDir
+
+declare nextStashKey=0
+declare -A stash=()
 
 main() {
   local type block
@@ -14,107 +18,234 @@ main() {
 
   while hear type rest; do
     case "$type" in
-				peekBlock)
-						peekBlock $rest
-						;;
-
-				putData)
-						putData $rest
-            ;;
+				peek) peek $rest;;
+				put) put $rest;;
+				release) release $rest;;
+				stageFile) stageFile $rest;;
     esac
 
     say "@YIELD"
   done
 }
 
-peekBlock() {
-	local header line hash cacheFile foundHeader
-	local -A stash=()
+peek() {
+	local key line hash cacheFile foundKey
+
+	set -x
 
 	# header should be the block id, followed by input bindings
 	# but this is all opaque to us
-	header=$(
-		while hear line && [[ ! -z $line ]]; do
+	key=$(
+		while hear line && [[ ! -z "$line" ]]; do
 			echo "$line"
 		done
-	)
+				)
 
-	hash=$(sha1sum <<< "$header")
-	cacheFile="$dataDir/${hash%% *}"
+	hash="$(sha1sum <<< "$key")"
+	hash="${hash%% *}"
+	cacheFile="$blockDir/${hash}"
+	missed=
 
 	if [[ -e "$cacheFile" ]]; then
 		{
-			foundHeader=$(
-				while read line && [[ ! -z $line ]]; do
-					echo "$line"
-				done
-			)
+			while read type rest; do
+				case $type in
+						KEY)
+								foundKey=$(
+									while read line && [[ "$line" != ';' ]]; do
+										echo "$line"
+									done
+								)
 
-			if [[ "$foundHeader" == "$header" ]]; then
+								if [[ "$foundKey" != "$key" ]]; then
+									missed=1
+									break
+								fi
+								;;
+
+						FILE)
+								fn="${rest@P}"
+
+								if [[ ! -e "$fn" ]]; then
+									missed=1
+									break
+								fi
+								;;
+
+						OUT)
+								break
+								;;
+				esac
+			done
+
+			if [[ ! $missed ]]; then
 				say "hit"
 
-				while read line && [[ -z $line ]]; do
+				while read line; do
 					say "$line"
 				done
 
 				say
-				say "@YIELD"
+				return
 			fi
 		} <"$cacheFile"
-
-		return
 	fi
 
+	token="_${nextStashKey}"
+	nextStashKey=$((nextStashKey+1))
+	stash["$token"]="$(cat <<EOF
+$cacheFile
+KEY
+$key
+;
+HASH ${hash}
+CREATED ${EPOCHSECONDS}
+EOF
+)"
+
 	say "miss"
-	# todo.....  pass some token back to runner so that we don't have to
-	# got through the header/hash rigmarole again
+	say "$token"
 }
 
-# todo below should take name hint
-# and not have anything to do with hashing etc
-# (could take a cache token?)
+put() {
+	local hash cacheFile
+	local token="$1"
+	local stashed="${stash[$token]}"
 
-putData() {
-	local line header hash cacheFile dataFile dataFileNum specs
-	local -a dataFiles=()
-	local -a specs=()
+	if [[ ! -z "$stashed" ]]; then
+		{
+			read cacheFile
 
-	header=$(
-		while hear line && [[ ! -z $line ]]; do
-			echo "$line"
-		done
-	)
+			{
+				while read type rest; do
+					case "$type" in
+							KEY)
+									echo "KEY"
+									while read line; do
+										echo "$line"
+										[[ $line == ';' ]] && break
+									done
+							;;
+							STAGEDFILE)
+									read fn tmpFile <<<"$rest"
+									mv "$tmpFile" "${fn@P}"
+									echo "FILE $fn"
+							;;
+					esac
+				done
 
-	hash=$(sha1sum <<< "$header")
-	cacheFile="$dataDir/${hash%% *}"
-	dataFileNum=0
-
-	{
-		echo "$header" 
-
-		echo
-
-		while hear line && [[ $line != "fin" ]]; do
-			dataFile="${cacheFile}.${dataFileNum}.data"
-
-			say "$dataFile"
-			say "@YIELD"
-
-			hear
-
-			if [[ -e "$dataFile" ]]; then
-				echo "$dataFile"
-				lastMod=$(stat --format=%Y "$dataFile")
-				specs+=("file;$dataFile;$lastMod")
-			fi
-
-			fileNum=$((dataFileNum+1))
-		done
-	} >"$cacheFile"
-
-	echo HELLO >&2
-
-  IFS=| say "${specs[*]}"
+				while hear line && [[ ! -z "$line" ]]; do
+					echo "$line"
+				done
+			} >"$cacheFile"
+		} <<<"$stashed"
+	fi
 }
+
+release() {
+	local token="$1"
+	unset stash["$token"]
+
+	#todo delete tmp file
+}
+
+stageFile() {
+	local token name stashed fn ext dataFile tmpFile hash created
+	token="$1"
+	name="$2"
+	stashed="${stash[$token]}"
+
+	if [[ ! -z $stashed ]]; then
+		{
+			read _
+
+			while read type rest; do
+				case "$type" in
+						HASH)
+								hash="$rest"
+								;;
+						CREATED)
+								created="$rest"
+								break #always after HASH
+								;;
+				esac
+			done
+		} <<<"$stashed"
+
+		fn="\${dataDir}/${name%.*}.${hash}.${created:-0}"
+		ext="${name##*.}"
+
+		if [[ ! -z "$ext" ]]; then
+			fn+=".${ext}"
+		fi
+
+		tmpFile=$(mktemp -u)
+
+		say "$tmpFile"
+		say "@YIELD"
+
+		hear
+
+		if [[ -e "$tmpFile" ]]; then
+			stash[$token]+=$'\n'"STAGEDFILE $fn $tmpFile"
+			lastMod=$(stat --format=%Y "$tmpFile")
+			say "file;$fn;$lastMod"
+		else
+			echo "cache data file not written!" >&2
+			say
+		fi
+	fi
+}
+
+
+
+
+
+# # todo below should take name hint
+# # and not have anything to do with hashing etc
+# # (could take a cache token?)
+
+# putData() {
+# 	local line header hash cacheFile dataFile dataFileNum specs
+# 	local -a dataFiles=()
+# 	local -a specs=()
+
+# 	header=$(
+# 		while hear line && [[ ! -z $line ]]; do
+# 			echo "$line"
+# 		done
+# 	)
+
+# 	hash=$(sha1sum <<< "$header")
+# 	cacheFile="$dataDir/${hash%% *}"
+# 	dataFileNum=0
+
+# 	{
+# 		echo "$header" 
+
+# 		echo
+
+# 		while hear line && [[ $line != "fin" ]]; do
+# 			dataFile="${cacheFile}.${dataFileNum}.data"
+
+# 			say "$dataFile"
+# 			say "@YIELD"
+
+# 			hear
+
+# 			if [[ -e "$dataFile" ]]; then
+# 				echo "$dataFile"
+# 				lastMod=$(stat --format=%Y "$dataFile")
+# 				specs+=("file;$dataFile;$lastMod")
+# 			fi
+
+# 			fileNum=$((dataFileNum+1))
+# 		done
+# 	} >"$cacheFile"
+
+# 	echo HELLO >&2
+
+#   IFS=| say "${specs[*]}"
+# }
 
 main "$@"
